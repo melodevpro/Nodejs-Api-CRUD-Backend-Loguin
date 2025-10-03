23756dcefa98693eca67b5466e949a357a7402fa
melomatthewdev <devscustomers@outlook.com>
2025-10-03 15:00:16 -0500

    creando rutas json

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const respuesta = require('../../red/respuestas');
+
+
 router.get('/', function (req, res) {
-    res.send ('Rutas de clientes')
+    respuesta.success(req, res, 'Todo ok desde clientes', 200);
 });
 
 module.exports = router;
